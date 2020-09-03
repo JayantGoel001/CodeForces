8a153173aa1fd364e809e7355071402efd7a91c8
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-03 07:06:33 +0530

    Initial Commit

--- SoftDrinking.cpp
+++ SoftDrinking.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
 
+    int x = (k*l)/nl;
+    int y = c*d;
+    int z = p/np;
+
+    cout<<min(min(x,y),z)/n;
 }
