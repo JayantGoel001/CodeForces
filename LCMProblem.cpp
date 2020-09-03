ada8d03fc413462a68ae12cd2734c0c0d5f3a0f6
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-03 07:25:32 +0530

    Initial Commit

--- LCMProblem.cpp
+++ LCMProblem.cpp
@@ -1,30 +1,5 @@
 #include<iostream>
 using namespace std;
-int gcd(int x,int y){
-    if(x==1 || y==1){
-        return 1;
-    }
-    else if(x==0){
-        return y;
-    }
-    else if (y==0){
-        return x;
-    }
-    else{
-        if(x>y){
-            return gcd(x-y,y);
-        }
-        else{
-            return gcd(x,y-x);
-        }
-    }
-}
-bool inRange(int x,int l,int r){
-    if(x>=l && x<=r){
-        return true;
-    }
-    return false;
-}
 int main(){
     int n;
     cin>>n;
