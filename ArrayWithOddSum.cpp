637b36b97a09505fecd17325a3a1161811c68eec
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-04 05:28:23 +0530

    Initial Commit

--- ArrayWithOddSum.cpp
+++ ArrayWithOddSum.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n;
+        cin>>n;
+        int *ar = new int[n];
+        int oddNum = 0;
+        int sum=0;
+        for (int j=0;j<n;j++){
+            cin>>ar[j];
+            if(ar[j]%2==1){
+                oddNum++;
+            }
+            sum+=ar[j];
+        }
 
+        if((n%2==1 && oddNum%2==1) || sum%2==1){
+            cout<<"YES"<<endl;
+        } else{
+            cout<<"NO"<<endl;
+        }
+
+    }
 }
