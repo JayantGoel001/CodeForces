197830c326f327869bba125afaabbc85f66635ca
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-03 17:47:23 +0530

    Initial Commit

---
+++ OmkarAndCompletion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    for(int i=0;i<T;i++){
+        int n;
+        cin>>n;
+        for(int j=0;j<n;j++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+}
