55ad73ed02f3f6105b957ff24b0b48f3611ed8d3
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-04 05:52:54 +0530

    Initial Commit

--- BlackSquare.cpp
+++ BlackSquare.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+    int *ar = new int[5];
+    for(int i=1;i<5;i++){
+        cin>>ar[i];
+    }
+    string str;
+    cin>>str;
+    int sum=0;
+    for(char i : str){
+        sum+=ar[int(i)-48];
+    }
+    cout<<sum;
 }
