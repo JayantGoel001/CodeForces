9d7204ff4dd4f06e4db4b225b42e7c86ac48fd7f
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-07 14:04:02 +0530

    Initial Commit

--- Tprimes.cpp
+++ Tprimes.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int tot = 1000000;
+    int tot = 1000000+1;
     bool *isPrime = new bool[tot];
     memset(isPrime,true,tot);
+    isPrime[1]=false;
     for (int i = 2; i*i < tot; ++i) {
         for (int j = i*i; j < tot; j+=i) {
             isPrime[j]= false;
