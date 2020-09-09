560b4605a7b09a2dd092977e3f55fb7e2f4605f0
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-09 13:49:47 +0530

    Initial Commit

---
+++ CollectingCoins.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int a,b,c,n;
+        cin>>a>>b>>c>>n;
+        if(max(a,max(b,c))<n && (a+b+c+n)%3==0){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+
+}
+
