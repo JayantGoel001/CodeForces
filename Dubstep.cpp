37b3e1b4ffedbbaacbe11c481da22377e8a3e9b2
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-10 21:59:15 +0530

    Initial Commit

---
+++ Dubstep.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    vector<string> v;
+    int x =str.find("WUBWUB");
+    while (x!=string::npos){
+        if (x==0){
+            str.replace(x,3,"");
+        }else {
+            str.replace(x, 3, "");
+        }
+        x= str.find("WUBWUB");
+    }
+    x = str.find("WUB");
+    while (x!=string::npos) {
+        if (x==0){
+            str.replace(x,3,"");
+        }
+        else {
+            str.replace(x, 3, " ");
+        }
+        x= str.find("WUB");
+    }
+    cout<<str;
+}
