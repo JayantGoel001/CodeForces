530a10b4e51f721d90d05b795b078bce05b39d67
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-10 12:30:44 +0530

    Initial Commit

--- NewYearAndHurry.cpp
+++ NewYearAndHurry.cpp
@@ -7,7 +7,11 @@ int main(){
     cin>>n>>k;
     int tot = 240;
     tot-=k;
-    tot = (tot - tot%5)/n;
+    tot = (tot - tot%5)/5;
     int output = (sqrt(1+8*tot) - 1)/2;
-    cout<<output;
+    if(output>n){
+        cout<<n;
+    }else{
+        cout<<output;
+    }
 }
