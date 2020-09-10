62dd11e372a08825c4a8967df1e81cb9fb54a772
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-10 06:34:14 +0530

    Initial Commit

---
+++ Presents.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int *ar= new int[n];
+    map<int,int> m;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin>>x;
+        m.insert(pair<int,int>(x,i+1));
+    }
+    for(auto x=m.begin();x!=m.end();x++){
+        cout<<x->second<<" ";
+    }
+
+}
