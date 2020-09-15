4595d09ac9d66ffa6f944206c8bc275813e1fd5e
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-15 23:10:35 +0530

    Initial Commit

---
+++ TwoArraysAndSwaps.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n,k;
+        cin>>n>>k;
+        int *ar = new int[n];
+        int *br = new int[n];
+        int max = INT16_MIN;
+        int sum = 0;
+
+        for (int j = 0; j < n; ++j) {
+            cin>>ar[j];
+            if(max<ar[j]){
+                max= ar[j];
+            }
+            sum+=ar[j];
+        }
+        for (int j = 0; j < n; ++j) {
+            cin>>br[j];
+        }
+
+        sort(ar,ar+n);
+        sort(br,br+n);
+        vector<int> v;
+        for (int j = 0; j < n; ++j) {
+            if(br[j]>max){
+                v.push_back(br[j]);
+            }
+        }
+        int j=0;
+        while (k!=0 && !v.empty()){
+            sum+=v[v.size()-1];
+            v.pop_back();
+            sum-=ar[j];
+            j++;
+            k--;
+        }
+        cout<<sum<<"\n";
+
+
+    }
+}
