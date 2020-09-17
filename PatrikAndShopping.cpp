55f51148cc661e68313079752f7a277e119f93b6
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-17 16:53:49 +0530

    Initial Commit

--- PatrikAndShopping.cpp
+++ PatrikAndShopping.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 int main(){
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a+b<c){
-        cout<<(a+b)*2;
-    }
-    else{
-        cout<<a+b+c;
+    int *ar = new int[3];
+    cin>>ar[0]>>ar[1]>>ar[2];
+
+    sort(ar,ar+3);
+
+    if(ar[0]+ar[1]<ar[2]){
+        cout<<(ar[0]+ar[1])*2;
+    }else{
+        cout<<(ar[0]+ar[1]+ar[2]);
     }
 }
