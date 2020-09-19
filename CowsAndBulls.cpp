c3855d83fdefc4d2703c264eb21af8d851601006
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-19 12:09:46 +0530

    Initial Commit

--- CowsAndBulls.cpp
+++ CowsAndBulls.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(){
-//    string secretNumber = to_string(rand() % 10000);
-    string secretNumber  = "9432";
+    string secretNumber = to_string(rand() % 10000);
     cout<<"Choose A Secret Number(Between 1000 And 9999): \nStart Guessing ....\n";
     int count=0;
+
     map<char,vector<int>> secretNumberMap;
     for (int i = 0; i < 4; ++i) {
         if (secretNumberMap.count(secretNumber[i]) == 0){
@@ -16,8 +17,10 @@ int main(){
         secretNumberMap[secretNumber[i]].push_back(i);
     }
 
+
     string str;
     while (true){
+        map<char,vector<int>> temp=secretNumberMap;
         cin>>str;
         if (str.length()!=4){
             cout<<"Invalid Input!\n";
@@ -28,16 +31,29 @@ int main(){
 
         for (int i = 0; i < 4; ++i) {
             char x = str[i];
-
+            if (secretNumberMap.count(x)!=0){
+                vector<int> key = secretNumberMap[x];
+                auto it = find(key.begin(),key.end(),i);
+                if (it!=key.end()){
+                    bulls++;
+                    key.erase(it);
+                    secretNumberMap[x] = key;
+                }
+            }
+        }
+        for (int i = 0; i < 4; ++i) {
+            char x = str[i];
+            if (secretNumberMap.count(x)!=0 && secretNumberMap[x].size()!=0){
+               cows++;
+            }
         }
 
+        count++;
         if (bulls==4) {
             cout<<"Congratulations, You did It.\nYou took "<<count<<" to guess the number.\nSecret Number is "<<secretNumber<<"\n";
             break;
         }
-        count++;
-
         cout<<"Turn "<<count<<" : Your Guess "<<str<<" has "<<bulls<<" Bulls and "<<cows<<" cows.\n";
-        break;
+        secretNumberMap=temp;
     }
 }
