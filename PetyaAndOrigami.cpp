d70394b00de8dd804829c94e128f2f073ac70fc5
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-20 14:23:28 +0530

    Initial Commit

--- PetyaAndOrigami.cpp
+++ PetyaAndOrigami.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+
+long long int getSheets(long long int x, long long int k);
+
 using namespace std;
 int main(){
     long long int n,k;
     cin>>n>>k;
-    long long int output = ((n*15)%k)==0?(n*15)/k:(n*15)/k+1;
+    long long int output;
+    long long int x = n*2;
+    long long int y = n*5;
+    long long int z = n*8;
+    output = getSheets(x,k)+getSheets(y,k)+getSheets(z,k);
     cout<<output;
+}
+
+long long int getSheets(long long int x, long long int k) {
+    return  (x%k)==0?(x/k):(x/k+1);
 }
