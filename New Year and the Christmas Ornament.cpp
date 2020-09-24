97df5847ec9bf5f4cd5a2630c6278bbbf81f4ac5
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-25 03:46:56 +0530

    Initial Commit

--- New Year and the Christmas Ornament.cpp
+++ New Year and the Christmas Ornament.cpp
@@ -3,24 +3,13 @@ using namespace std;
 int main(){
     int y,b,r;
     cin>>y>>b>>r;
-    int mini=min(y,min(b,r));
-    if (mini==y){
-        if (y+1<=b && y+2<=r){
-            cout<<3*(y+1);
-        }else{
-            cout<<3*y;
-        }
-    }else if(mini==b){
-        if (b-1<=y && b+1<=r){
-            cout<<3*b;
-        }else{
-            cout<<3*(b-1);
-        }
-    }else{
-        if (r-2<=y && r-1<=b){
-            cout<<3*(r-1);
-        }else{
-            cout<<3*(r-2);
-        }
+    int i=1;
+    int j=2;
+    int k=3;
+    while (i<=y && j<=b && k<=r){
+        i++;
+        j++;
+        k++;
     }
+    cout<<(i+j+k)-3;
 }
