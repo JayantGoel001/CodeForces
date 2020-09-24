cc92e2880986c5beee0522fe84a2be52130c326f
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-25 04:29:33 +0530

    Initial Commit

---
+++ Diverse Team.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <list>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int *ar = new int[n];
+    map<int,list<int>> m;
+    for (int i = 0; i < n; ++i) {
+        cin>>ar[i];
+        if (m.count(ar[i])==0){
+            m.insert(make_pair(ar[i],list<int>()));
+        }
+        m[ar[i]].push_back(i+1);
+    }
+    if (k<=m.size()){
+        cout<<"YES\n";
+        for (auto it=m.begin();it!=m.end();it++){
+            cout<<(it->second).front()<<" ";
+        }
+    } else{
+        cout<<"NO\n";
+    }
+}
