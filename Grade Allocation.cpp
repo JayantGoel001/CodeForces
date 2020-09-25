505c2f06fcd1d4995860c2edc3cb9fe42e8c3c93
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-26 00:59:58 +0530

    Initial Commit

---
+++ Grade Allocation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n,m;
+        cin>>n>>m;
+        int sum=0;
+        for (int j = 0; j < n; ++j) {
+            int x;
+            cin>>x;
+            sum+=x;
+        }
+        cout<<m<<"\n";
+    }
+}
