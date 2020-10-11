24de8441116e24c8e39ebada6bb84c8e7907197d
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-11 16:57:23 +0530

    Initial Commit

---
+++ Three Strings.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        string a,b,c;
+        cin>>a;
+        cin>>b;
+        cin>>c;
+        int len = a.length();
+        bool isPossible = true;
+        for (int j = 0; j < len; ++j) {
+            if (a[j]==b[j] || (a[j]!=c[j] && b[j]!=c[j])){
+                cout<<"NO\n";
+                isPossible= false;
+                break;
+            }
+        }
+        if(isPossible){
+            cout<<"YES\n";
+        }
+    }
+}
