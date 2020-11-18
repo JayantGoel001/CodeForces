9cf551248aa125799a4420d13a49f999955f8f9e
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-18 19:33:24 +0530

    Initial Commit

---
+++ Sum of Medians.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n,k;
+        cin>>n>>k;
+    }
+}
