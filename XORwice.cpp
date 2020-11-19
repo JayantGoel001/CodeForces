0d046c60cee384ead31cdb7f30c31e180bf05f92
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-19 07:52:24 +0530

    Initial Commit

--- XORwice.cpp
+++ XORwice.cpp
@@ -1,30 +1,12 @@
 #include <iostream>
 #define ll long long int
 using namespace std;
-ll GCD(int a,int b){
-    if (a==0){
-        return b;
-    }
-    if (b==0){
-        return a;
-    }
-    if (a==1 || b==1){
-        return 1;
-    }
-    if (a>b){
-        return GCD(a-b,b);
-    } else{
-        return GCD(a,b-a);
-    }
-}
 int main(){
     int t;
     cin>>t;
     for (int i = 0; i < t; ++i) {
         ll a,b;
         cin>>a>>b;
-        ll x = GCD(a,b);
-        cout<<x<<" a";
-        cout<<(a^x)+(b^x)<<"\n";
+        cout<<(a^b)<<"\n";
     }
 }
