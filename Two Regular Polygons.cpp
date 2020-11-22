0057d018cd3cf3d89a32df22ef47069ff8b2e79c
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-22 16:44:36 +0530

    Initial Commit

--- Two Regular Polygons.cpp
+++ Two Regular Polygons.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-    int n,m;
-    cin>>n>>m;
-    if (n%m==0){
-        cout<<"YES\n";
-    } else{
-        cout<<"NO\n";
+    for (int i = 0; i < t; ++i) {
+        int n,m;
+        cin>>n>>m;
+        if (n%m==0){
+            cout<<"YES\n";
+        } else{
+            cout<<"NO\n";
+        }
     }
 }
