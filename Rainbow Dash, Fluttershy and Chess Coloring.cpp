a7721d33bdb28150302733a4525dfc7db4ab38cd
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-22 12:20:48 +0530

    Initial Commit

---
+++ Rainbow Dash, Fluttershy and Chess Coloring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n;
+        cin>>n;
+        cout<<n/2+1<<"\n";
+    }
+}
