8bd6706302680e8886b59491bb1391a8bf2ffdd2
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-30 20:10:29 +0530

    Initial Commit

---
+++ Strange Functions.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#define ll long long int
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        string str;
+        cin>>str;
+        cout<<str.length()<<"\n";
+    }
+}
