32bab0baf3855da3c9d5c13695cc38757f708b9f
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-09 09:13:45 +0530

    Initial Commit

---
+++ A+B (Trial Problem).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+}
