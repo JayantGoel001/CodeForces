376a3d12c6631c3e16c75cd798e36b2d9aa2d2da
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-10 05:50:31 +0530

    Initial Commit

---
+++ Eleven.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool isFibonacci[1001];
+    for (int i = 1; i <= 1000; ++i) {
+        isFibonacci[i] = false;
+    }
+    isFibonacci[1]= true;
+    isFibonacci[2]= true;
+    int n;
+    cin>>n;
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    while (c<=n){
+        c = a+b;
+        a = b;
+        b = c;
+        if (c<n) {
+            isFibonacci[c] = true;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (isFibonacci[i+1]){
+            cout<<"O";
+        } else{
+            cout<<"o";
+        }
+    }
+}
