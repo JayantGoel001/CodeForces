ced7e081dc8c3c72c4db4c54c506c1a5c730d9f2
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-14 10:56:23 +0530

    Initial Commit

--- VanyaAndCards.cpp
+++ VanyaAndCards.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include <map>
+#include <cmath>
 using namespace std;
 int main(){
     int n,x;
@@ -8,19 +8,9 @@ int main(){
     for (int i = 0; i < n; ++i) {
         cin>>ar[i];
     }
-    map<int,int> m;
+    float sum = 0.0;
     for (int i = 0; i < n; ++i) {
-        if (m.count(-1*ar[i])!=0){
-            m[-1*ar[i]] =m[-1*ar[i]]-1;
-        } else if(m.count(ar[i])!=0){
-            m[ar[i]] = m[ar[i]] + 1;
-        } else{
-            m.insert(make_pair(ar[i],1));
-        }
+        sum+=ar[i];
     }
-    int count = 0;
-    for(auto it = m.begin();it!=m.end();it++){
-        count+=it->second;
-    }
-    cout<<count;
+    cout<<ceil(abs(sum)/x);
 }
