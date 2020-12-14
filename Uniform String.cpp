68e659b28a453f5a3cdbe135adfb8b4a6376fd1c
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-14 21:37:11 +0530

    Initial Commit

---
+++ Uniform String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n,k;
+        cin>>n>>k;
+        int times = n/k;
+        int j=0;
+        char ch = 'a';
+        while (j<n%k){
+            cout<<ch;
+            j++;
+        }
+        while (j<n){
+            for (int l = 0; l < times && j<n; ++l) {
+                cout<<ch;
+                j++;
+            }
+            ch +=1;
+        }
+        cout<<'\n';
+    }
+}
