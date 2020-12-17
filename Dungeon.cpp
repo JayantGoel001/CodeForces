8a1f3e57795c03b598110d8d9864892f0b89e874
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-17 21:17:56 +0530

    Initial Commit

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -7,12 +7,12 @@ int main(){
     for (int i = 0; i < t; ++i) {
         ll a,b,c;
         cin>>a>>b>>c;
-        float x = (a+b+c)/7.0;
-        if (x<min(a,min(b,c))){
-            a-=(int)x;
-            b-=(int)x;
-            c-=(int)x;
-            if ((a+b+c)%6==0){
+        int x = (a+b+c)/7;
+        if (x<=min(a,min(b,c))){
+            a-=x;
+            b-=x;
+            c-=x;
+            if ((a+b+c+x)%7==0){
                 cout<<"YES\n";
             } else{
                 cout<<"NO\n";
