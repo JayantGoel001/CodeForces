2722cec0b97bb406630599b83ec5af8ae468bd3b
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-20 21:20:10 +0530

    Initial Commit

--- Fair Numbers.cpp
+++ Fair Numbers.cpp
@@ -4,15 +4,6 @@
 #define ll long long int
 
 using namespace std;
-
-int getGCD(vector<int> vector) {
-    int gcd = vector[0];
-    for (int i = 1; i <vector.size() ; ++i) {
-        gcd = __gcd(gcd,vector[i]);
-    }
-    return gcd;
-}
-
 bool isFair(ll n) {
     ll num = n;
     while (num!=0){
@@ -31,21 +22,8 @@ int main(){
     for (int i = 0; i < t; ++i) {
         ll n;
         cin>>n;
-        ll num = n;
-        vector<int> v;
-        while (num!=0){
-            int x = num%10;
-            if (x!=0 && n%x!=0){
-                v.push_back(n%x);
-            }
-            num/=10;
-        }
-        if (v.size()!=0){
-            int gcd = getGCD(v);
-            cout<<gcd<<"\n";
-            while (!isFair(n)) {
-                n += gcd;
-            }
+        while (!isFair(n)) {
+            n += 1;
         }
         cout<<n<<"\n";
     }
