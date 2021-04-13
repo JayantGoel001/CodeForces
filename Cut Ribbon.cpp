1b816f5c7fca749e0be463da0ad88d08abe231ee
JayantGoel001 <jgoel92@gmail.com>
2021-04-13 16:41:00 +0530

    Initial Commit

--- Cut Ribbon.cpp
+++ Cut Ribbon.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-    float n,a,b,c;
+    int n,a,b,c;
     cin>>n>>a>>b>>c;
     int maxi = -1;
     int x = n/min(min(a,b),c);
     for(int i=x;i>=0;i--){
-        for(int j=0;j<=i;j++){
-            int k = abs((n-a*i + b*j)/c);
+        for(int j=0;j<=x;j++){
+            int k = abs((n-a*i - b*j)/c);
             if (a*i + b*j + c*k==n){
                 maxi = max(maxi,i+j+k);
             }
