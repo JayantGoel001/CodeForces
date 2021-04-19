8fa99c043c5c0bb7e655a7d93f60f1f10f0166b6
JayantGoel001 <jgoel92@gmail.com>
2021-04-19 18:52:29 +0530

    Initial Commit

--- The Sports Festival.cpp
+++ The Sports Festival.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin>>n;
     ll ar[n];
@@ -11,18 +13,21 @@ int main(){
         cin>>ar[i];
     }
     sort(ar,ar+n);
-    ll d = 0;
-    int z = n-1;
-    int a = 0;
-    for(int i=0;i<n;i++) {
-        d += abs(ar[z] - ar[a]);
-        ll x = abs(ar[z-1] - ar[a]);
-        ll y = abs(ar[z] - ar[a+1]);
-        if (x<y){
-            z--;
-        } else{
-            a++;
+
+    ll dp[n][n];
+    dp[0][0]= ar[n-1] - ar[0];
+    for (int i = 1; i < n; ++i) {
+        dp[0][i] = dp[0][i-1] + ar[n-i-1] - ar[0];
+        dp[i][0] = dp[i-1][0] + ar[n-1] - ar[i];
+    }
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < n-i; ++j) {
+            dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + ar[n-j-1] - ar[i];
         }
     }
-    cout<<d<<"\n";
+    ll ans = LLONG_MAX;
+    for (int i = 0; i < n; ++i) {
+        ans = min(ans,dp[i][n-i-1]);
+    }
+    cout<<ans;
 }
