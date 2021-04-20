b1717aa18913c3347a8f06ae5e2c615ac5e184d2
JayantGoel001 <jgoel92@gmail.com>
2021-04-20 21:07:11 +0530

    Initial Commit

--- Product 1 Modulo N.cpp
+++ Product 1 Modulo N.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
 #define ll long long int
 using namespace std;
+int GCD(int a,int b){
+    if(b==0){
+        return a;
+    }
+    return GCD(b,a%b);
+}
 int main(){
     ll n;
     cin>>n;
+    vector<ll> v;
+    ll prod = 1;
 
+    for (int i = 1; i < n; ++i) {
+        if (GCD(i,n)==1){
+            v.push_back(i);
+            prod = (prod*i)%n;
+        }
+    }
+    if (prod!=1){
+        v.pop_back();
+    }
+    cout<<v.size()<<"\n";
+    for (int i = 0; i < v.size(); ++i) {
+        cout<<v[i]<<" ";
+    }
 }
