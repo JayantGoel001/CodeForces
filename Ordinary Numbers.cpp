d94b63c6588d8ab6eb5679f86c96bab3c36bc33c
JayantGoel001 <jgoel92@gmail.com>
2021-05-05 20:58:31 +0530

    Initial Commit

--- Ordinary Numbers.cpp
+++ Ordinary Numbers.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 #include <valarray>
-#include <map>
 #define ll long long int
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    map<int,ll> mp;
-    mp[0] = 1;
-    mp[1] = 11;
-    mp[2] = 111;
-    mp[3] = 1111;
-    mp[4] = 11111;
-    mp[5] = 111111;
-    mp[6] = 1111111;
-    mp[7] = 11111111;
-    mp[8] = 111111111;
     int t;
     cin>>t;
     while (t--){
         ll n;
         cin>>n;
-        int prev = 0;
-        for (int i = 1; i <= n; ++i) {
-            int x = log(i) / log(10);
-            if (i % mp[x] == 0) {
-                prev++;
-            }
+        int base = 1;
+        int result = 0;
+        int target;
+        for (int i = 10; i <=n ; i*=10) {
+            target = i-1;
+            result+=target/base;
+            base = base*10+1;
         }
-        cout<<prev<<"\n";
+        result += n/base;
+        cout<<result<<"\n";
     }
 }
