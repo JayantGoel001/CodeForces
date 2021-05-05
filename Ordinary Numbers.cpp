3047cf94d8978dba836afb5239a27451ec18d5f6
JayantGoel001 <jgoel92@gmail.com>
2021-05-05 20:50:50 +0530

    Initial Commit

--- Ordinary Numbers.cpp
+++ Ordinary Numbers.cpp
@@ -3,12 +3,11 @@
 #include <map>
 #define ll long long int
 using namespace std;
-map<int,ll> mp;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    ll TOT = (ll)pow(10,9);
 
+    map<int,ll> mp;
     mp[0] = 1;
     mp[1] = 11;
     mp[2] = 111;
@@ -18,24 +17,18 @@ int main(){
     mp[6] = 1111111;
     mp[7] = 11111111;
     mp[8] = 111111111;
-
-    ll ar[TOT+1];
-    ar[0]=0;
-    ll prev = 0;
-    for (int i = 1; i <= TOT; ++i) {
-        int x = log(i) / log(10);
-        if (i % mp[x] == 0) {
-            ar[i] = prev + 1;
-            prev = ar[i];
-        } else{
-            ar[i] = prev;
-        }
-    }
     int t;
     cin>>t;
     while (t--){
         ll n;
         cin>>n;
-        cout<<ar[n]<<"\n";
+        int prev = 0;
+        for (int i = 1; i <= n; ++i) {
+            int x = log(i) / log(10);
+            if (i % mp[x] == 0) {
+                prev++;
+            }
+        }
+        cout<<prev<<"\n";
     }
 }
