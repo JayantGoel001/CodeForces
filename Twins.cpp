07b96f828044b1a0be24a184e61a8357b2ee579d
JayantGoel001 <jgoel92@gmail.com>
2021-06-04 10:38:49 +0530

    Initial Commit

---
+++ Twins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+#define int long long int
+using namespace std;
+int32_t main(){
+    int t=1;
+//    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        int ar[n];
+        int remainingSum = 0;
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+            remainingSum+=ar[i];
+        }
+        sort(ar,ar+n,greater<int>());
+        int sum =0;
+        int i;
+        for (i = 0; i < n; ++i) {
+            sum+=ar[i];
+            remainingSum-=ar[i];
+            if (sum>remainingSum){
+                break;
+            }
+        }
+        cout<<i+1<<"\n";
+    }
+}
