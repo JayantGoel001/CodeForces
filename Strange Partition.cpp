59c1b9f038459962b5cb7f3e00be7c20193fb527
JayantGoel001 <jgoel92@gmail.com>
2021-07-03 16:55:50 +0530

    Initial Commit

---
+++ Strange Partition.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin >> n>>k;
+        int ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+        }
+        int mini = 0;
+        int maxi = 0;
+        for (int i = 0; i < n; ++i) {
+            mini +=  ar[i];
+            maxi += (int)ceil((double )ar[i]/k);
+        }
+
+        cout << (int)ceil((double )mini/k) <<" "<< maxi <<"\n";
+    }
+}
