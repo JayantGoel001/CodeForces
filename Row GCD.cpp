3ae68f3c0d3f2e5e725c12ce6588be74d09b7d17
JayantGoel001 <jgoel92@gmail.com>
2021-07-03 15:21:16 +0530

    Initial Commit

--- Row GCD.cpp
+++ Row GCD.cpp
@@ -1,35 +1,54 @@
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 
-#define ll long long int
+#define int unsigned long long int
+#define INF 1e18
+using namespace __gnu_pbds;
 using namespace std;
-int main(){
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n,m;
-    cin>>n>>m;
-    int ar[n];
-    int b[m];
-    for (int i = 0; i < n; ++i) {
-        cin>>ar[i];
-    }
-    for (int i = 0; i < m; ++i) {
-        cin>>b[i];
-    }
-    sort(ar,ar+n);
-    for (int i = 0; i < m; ++i) {
-        int x = b[i]+ar[0];
-        bool isTrue = true;
-        for (int j = 0; j < n; ++j) {
-            if ((ar[j]+b[i])%x!=0){
-                isTrue = false;
-                break;
-            }
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) {
+        int n,m;
+        cin >> n>>m;
+        int ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
         }
-        if (isTrue){
-            cout<<x<<" ";
-        } else{
-            cout<<1<<" ";
+        sort(ar,ar+n);
+        int gcd = 0;
+        for (int i = 1; i < n; ++i) {
+            gcd = GCD(gcd, ar[i] - ar[0]);
         }
+        for (int i = 0; i < m; ++i) {
+            int x;
+            cin>>x;
+            cout<<GCD(x+ar[0],gcd)<<" ";
+        }
+        cout << "\n";
     }
 }
