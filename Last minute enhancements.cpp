78d2e9d75fd43cde9848ed7c44448b4c7489d9f4
JayantGoel001 <jgoel92@gmail.com>
2021-07-03 13:47:52 +0530

    Initial Commit

--- Last minute enhancements.cpp
+++ Last minute enhancements.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,31 +13,13 @@ int main() {
         for (int j = 0; j < n; ++j) {
             cin>>ar[j];
         }
-        int size=  ar[n-1]+2;
-        int hash[size];
-        for (int j = 1; j <size ; ++j) {
-            hash[j] = 0;
-        }
-        for (int j = 0; j < n; ++j) {
-            hash[ar[j]]+=1;
-        }
-        for (int j = 1; j < size; ++j) {
-            if (hash[j]>1){
-                for (int k = j+1; k <size ; ++k) {
-                    if (hash[k]==0){
-                        hash[k]+=1;
-                        hash[j]-=1;
-                        break;
-                    }
-                }
+        map<int,int> mp;
+        for (int j = 0; j <n; ++j) {
+            if (mp.count(ar[j])){
+                ar[j]+=1;
             }
+            mp[ar[j]]++;
         }
-        int count = 0;
-        for (int j = 1; j <size ; ++j) {
-            if (hash[j]!=0){
-                count++;
-            }
-        }
-        cout<<count<<"\n";
+        cout<<mp.size()<<"\n";
     }
 };
