16196fe528634b41a04f89224f9aedccd90ea739
JayantGoel001 <jgoel92@gmail.com>
2021-07-10 22:15:09 +0530

    Initial Commit

--- 3.cpp
+++ 3.cpp
@@ -35,9 +35,71 @@ int32_t main() {
     int t = 1;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-
+        int k,n,m;
+        cin >> k>>n>>m;
+        int ar[n],br[m];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+        }
+        for (int i = 0; i < m; ++i) {
+            cin>>br[i];
+        }
+        bool exists = true;
+        vector<int> v;
+        int i,j;
+        i = 0;
+        j = 0;
+        while (i<n && j<m) {
+            int current = 0;
+            if (ar[i] == 0) {
+                v.push_back(ar[i++]);
+                k++;
+                continue;
+            } else if (br[j] == 0) {
+                v.push_back(br[j++]);
+                k++;
+                continue;
+            }else if (ar[i]<br[j]){
+                current = ar[i++];
+            } else{
+                current = br[j++];
+            }
+            if (current<=k){
+                v.push_back(current);
+            } else{
+                exists = false;
+                break;
+            }
+        }
+        while (i<n){
+            if (ar[i]==0){
+                k++;
+                v.push_back(ar[i++]);
+            }else if(ar[i]<=k){
+                v.push_back(ar[i++]);
+            } else{
+                exists = false;
+                break;
+            }
+        }
+        while (j<m){
+            if (br[j]==0){
+                k++;
+                v.push_back(br[j++]);
+            } else if (br[j]<=k){
+                v.push_back(br[j++]);
+            } else{
+                exists = false;
+                break;
+            }
+        }
+        if(exists){
+            for (int l = 0; l < v.size(); ++l) {
+                cout<<v[l]<<" ";
+            }
+        } else{
+            cout<<"-1";
+        }
         cout << "\n";
     }
 }
