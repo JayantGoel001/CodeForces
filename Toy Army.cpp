b99d7a9cccac33e539c78f9fe5fdc041d95c7c22
JayantGoel001 <jgoel92@gmail.com>
2021-07-22 22:39:19 +0530

    Initial Commit

---
+++ Toy Army.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define double double_t
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2, MOD) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout<<3 * n/2;
+        cout << "\n";
+    }
+}
