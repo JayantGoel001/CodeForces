a38d7af3b2906a6364ea5072923d448be1a69d36
Pranab Das <55971762+pranab99@users.noreply.github.com>
2021-07-28 01:56:51 +0530

    Create FindTheArray(1550A).cpp

---
+++ FindTheArray(1550A).cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
+using namespace std;
+
+float roundoff(float value, unsigned char prec) {
+	float pow_10 = pow(10.0f, (float)prec);
+	return round(value * pow_10) / pow_10;
+}
+
+
+
+
+void solve() {
+
+	int s;
+	cin>>s;
+
+	cout<<ceil(sqrt(s));
+
+}
+
+
+
+
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifdef Pranab
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	 cin>>t;
+
+	while(t--) {
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
+
+	return 0;
+}
+
